c587509d57744bc7122242ca2bc88ea2fcba9002
brajm008 <ujkkju117@outlook.com>
2022-09-30 20:34:37 +0930

    1.3.0.0 add config manager callback and sentry

--- MaxDrawDistancePlugin/MaxDrawDistance.cs
+++ MaxDrawDistancePlugin/MaxDrawDistance.cs
@@ -1,32 +1,77 @@
 ﻿using UnityEngine;
+using System;
 using BepInEx;
 using BepInEx.Configuration;
+using HarmonyLib;
 using JetBrains.Annotations;
 using ModdingTales;
 using PluginUtilities;
 using UnityEngine.SceneManagement;
+using Sentry;
+using static ConfigurationManager.ConfigurationManager;
+using BepInEx.Logging;
 
 namespace MaxDrawDistance
 {
     [BepInPlugin(Guid, "HolloFoxe's MaxDrawDistance", Version)]
     [BepInDependency(SetInjectionFlag.Guid)]
+    [BepInDependency(ConfigurationManager.ConfigurationManager.Guid)]
     public class MaxDrawDistance : BaseUnityPlugin
     {
         // constants
         private const string Guid = "org.hollofox.plugins.MaxDrawDistance";
-        private const string Version = "1.2.0.0";
+        private const string Version = "1.3.0.0";
+
+        internal static Harmony harmony;
+        internal static SentryOptions _sentryOptions = new SentryOptions
+        {
+            // Tells which project in Sentry to send events to:
+            Dsn = "https://06dc4d9a9d3a466b9c31839439ce487e@o1208746.ingest.sentry.io/4503901801873408",
+            Debug = true,
+            TracesSampleRate = 0.2,
+            IsGlobalModeEnabled = true,
+            AttachStacktrace = true
+        };
+
+        internal static Action<Scope> _scope = scope =>
+        {
+            scope.User = new User
+            {
+                Username = BackendManager.Username,
+            };
+            scope.Release = Version;
+        };
 
         private static ConfigEntry<ModdingUtils.LogLevel> LogLevelConfig { get; set; }
         private static ConfigEntry<float> MaxDraw { get; set; }
         private static ConfigEntry<float> MaxShadowDistance { get; set; }
+        internal static ConfigEntry<float> MaxLosDistance { get; set; }
+        internal static logToSentry useSentry => _useSentry.Value;
         
-        private static ModdingUtils.LogLevel LogLevel => LogLevelConfig.Value == ModdingUtils.LogLevel.Inherited ? ModdingUtils.LogLevelConfig.Value: LogLevelConfig.Value;
+        internal static ManualLogSource _logger;
+
+        private static ModdingUtils.LogLevel LogLevel => LogLevelConfig.Value == ModdingUtils.LogLevel.Inherited
+            ? ModdingUtils.LogLevelConfig.Value
+            : LogLevelConfig.Value;
 
         public void DoConfig(ConfigFile config)
         {
-            var logLevelDescription = new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true });
-            var maxDrawDescription = new ConfigDescription("", null, new ConfigurationManagerAttributes { CallbackAction = SetDrawDistance });
-            var maxShadowDescription = new ConfigDescription("", null, new ConfigurationManagerAttributes { CallbackAction = SetShadowDistance });
+            var logLevelDescription =
+                new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true });
+            var maxDrawDescription = new ConfigDescription("", null,
+                new ConfigurationManagerAttributes { CallbackAction = (o) =>
+                    {
+                        SentryInvoke(SetDrawDistance);
+                    }
+                });
+            var maxShadowDescription = new ConfigDescription("", null,
+                new ConfigurationManagerAttributes {
+                    CallbackAction = (o) =>
+                    {
+                        SentryInvoke(SetShadowDistance);
+                    }
+                });
+            // var maxLosDescription = new ConfigDescription("", null, new ConfigurationManagerAttributes { /*CallbackAction = LosPatch.UpdateFarClip*/});
 
             LogLevelConfig = config.Bind("Logging", "Log Level", ModdingUtils.LogLevel.Inherited, logLevelDescription);
 
@@ -35,29 +80,47 @@ namespace MaxDrawDistance
 
             MaxDraw = config.Bind("Draw Distance", "Render Distance", 3000f, maxDrawDescription);
             MaxShadowDistance = config.Bind("Draw Distance", "Shadow Distance", 3000f, maxShadowDescription);
+            // MaxLosDistance = config.Bind("Draw Distance", "Line of Sight Distance", 3000f, maxLosDescription);
 
             if (LogLevel >= ModdingUtils.LogLevel.High)
                 Logger.LogInfo("Config Bound");
         }
 
+        public void DoPatching()
+        {
+            harmony = new Harmony(Guid);
+            harmony.PatchAll();
+            if (LogLevel > ModdingUtils.LogLevel.None)
+                Logger.LogInfo($"Patched.");
+        }
+
         [UsedImplicitly]
         private void Awake()
         {
+            _logger = Logger;
+            SentryInvoke(Setup);
+        }
+
+        private void Setup()
+        {
             DoConfig(Config);
+            DoPatching();
+
+            _logger.LogEvent += logFowarding;
 
             if (LogLevel > ModdingUtils.LogLevel.None)
                 Logger.LogInfo("MaxDrawDistance Plug-in loaded");
-            
+
             ModdingUtils.Initialize(this, Logger);
             SetShadowDistance();
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
-        
-        private void SetDrawDistance(object o = null)
+
+        private void SetDrawDistance()
         {
             if (LogLevel > ModdingUtils.LogLevel.None)
                 Logger.LogInfo("Updating DrawDistance");
-            
+
             var cameras = FindObjectsOfType<Camera>();
             foreach (var cam in cameras)
             {
@@ -65,15 +128,34 @@ namespace MaxDrawDistance
             }
         }
 
-        public void SetShadowDistance(object o = null)
+        public void SetShadowDistance()
         {
             if (LogLevel > ModdingUtils.LogLevel.None)
                 Logger.LogInfo("Updating ShadowDistance");
             QualitySettings.shadowDistance = MaxShadowDistance.Value;
         }
 
-        private void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
-            => SetDrawDistance();
-        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            SetDrawDistance();
+            SetShadowDistance();
+        }
+
+        static void SentryInvoke(Action a) =>
+            ConfigurationManager.Utilities.Utils.SentryInvoke(a, _sentryOptions, _logger);
+
+        private void logFowarding(object o, LogEventArgs e)
+        {
+            if (useSentry != logToSentry.Enabled) return;
+            switch (e.Level)
+            {
+                case BepInEx.Logging.LogLevel.Fatal:
+                    SentrySdk.CaptureMessage(e.Data.ToString(), _scope, SentryLevel.Fatal);
+                    break;
+                case BepInEx.Logging.LogLevel.Error:
+                    SentrySdk.CaptureMessage(e.Data.ToString(), _scope, SentryLevel.Error);
+                    break;
+            }
+        }
     }
 }
---
+++ MaxDrawDistancePlugin/Patches/LosPatch.cs
@@ -0,0 +1,50 @@
+﻿using DataModel;
+using HarmonyLib;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace MaxDrawDistance.Patches
+{
+    //[HarmonyPatch(typeof(LineOfSightManager), "OnSetupInternals")]
+    /*[HarmonyPatch(typeof(LineOfSightManager), "StartProcess")]
+    public class LosPatch
+    {
+        private static Camera _captureCamera;
+
+        public static void Prefix(ref Camera ____captureCamera)
+        {
+            _captureCamera = ____captureCamera;
+            UpdateFarClip();
+        }
+
+        internal static void UpdateFarClip(object o = null)
+        {
+            Debug.Log("captureCamera.farClipPlane updating");
+            ViewPointManager.Instance.CaptureCamera.farClipPlane = MaxDrawDistance.MaxLosDistance.Value;
+            _captureCamera.farClipPlane = MaxDrawDistance.MaxLosDistance.Value;
+            Debug.Log("captureCamera.farClipPlane updated");
+        }
+    }
+
+    [HarmonyPatch(typeof(ViewPointManager), "RenderToCubemap")]
+    public class vpmPatch
+    {
+        public static bool Prefix(RenderTexture cubemap, float3 capturePos, ref Material ____tilePropOccluderMaterial, ref Mesh ____dummyFloorMesh, ref Material ____standardOccluderMaterial)
+        {
+            Debug.Log("RenderToCubemap Callback");
+
+
+            double shadowDistance = (double)QualitySettings.shadowDistance;
+            SharedBatchRenderer.SetBatchShadowRenderingState(false);
+            QualitySettings.shadowDistance = 0.0f;
+            ((BoardPresentation)BoardSessionManager.Board.BoardPresentation).CullAndRenderOccludersToCubemap(
+                ViewPointManager.Instance.CaptureCamera, capturePos, MaxDrawDistance.MaxLosDistance.Value, cubemap, 
+                ____tilePropOccluderMaterial, ____dummyFloorMesh, ____standardOccluderMaterial, 7);
+            QualitySettings.shadowDistance = (float)shadowDistance;
+            SharedBatchRenderer.SetBatchShadowRenderingState(true);
+
+            Debug.Log("RenderToCubemap done");
+            return false;
+        }
+    }*/
+}
