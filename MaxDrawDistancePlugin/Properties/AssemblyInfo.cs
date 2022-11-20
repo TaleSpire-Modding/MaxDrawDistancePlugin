44199914d72957a145486de66855536ffde41d4f
brajm008 <ujkkju117@outlook.com>
2022-11-20 15:37:38 +1030

    migrate to net48

--- MaxDrawDistancePlugin/Properties/AssemblyInfo.cs
+++ MaxDrawDistancePlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RForRotatePlugin")]
+[assembly: AssemblyTitle("Max Draw Distance")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("D20ArmyKnife")]
-[assembly: AssemblyProduct("RForRotatePlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("HolloFox")]
+[assembly: AssemblyProduct("Max Draw Distance")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(MaxDrawDistance.MaxDrawDistance.Version)]
+[assembly: AssemblyFileVersion(MaxDrawDistance.MaxDrawDistance.Version)]
